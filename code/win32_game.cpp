911fdaf58c1935026a7a6e1ecac7804598a74667
Ian Kachan <kachan.yanf@gmail.com>
2020-04-22 02:03:07 +0500

    Implemented the bitmap and the rectangle drawing by Windows in the main window

--- code/win32_game.cpp
+++ code/win32_game.cpp
@@ -1,10 +1,61 @@
 #include <windows.h>
 
-LRESULT CALLBACK 
-MainWindowCallback(HWND   Window,
-                   UINT   Message,
-                   WPARAM WParam,
-                   LPARAM LParam)
+#define local_persist static
+#define global_variable static
+#define internal static
+
+// Win32 prefix for functions meaning I defined those functions
+// and they're not Windows reserved names
+// Global variables
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext; 
+
+internal void Win32ResizeDIBSection(int Width, int Height)
+{
+    if(BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+
+    }
+    
+    if(!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32; // in fact we need 24 bits, 8 bits per each color (8 bits for RED, 8 bits for GREEN, 8 bits for BLUE)
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    
+
+    BitmapHandle = CreateDIBSection(
+        BitmapDeviceContext, &BitmapInfo,
+        DIB_RGB_COLORS,
+        &BitmapMemory,
+        0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(DeviceContext, 
+                  X, Y, Width, Height,
+                  X, Y, Width, Height,
+                  BitmapMemory,
+                  &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);
+}
+
+LRESULT CALLBACK  Win32MainWindowCallback(HWND   Window,
+                                          UINT   Message,
+                                          WPARAM WParam,
+                                          LPARAM LParam)
 {
     LRESULT Result = 0;
 
@@ -12,22 +63,26 @@ MainWindowCallback(HWND   Window,
     {
         case WM_SIZE:
         {
-            OutputDebugStringA("WM_SIZE\n");
-        } break;
-
-        case WM_DESTROY:
-        {
-            OutputDebugStringA("WM_DESTROY\n");
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
         } break;
 
         case WM_CLOSE:
         {
-            OutputDebugStringA("WM_CLOSE\n");
+            Running = false;
         } break;
 
         case WM_ACTIVATEAPP:
         {
             OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            Running = false;
         } break;
 
         case WM_PAINT:
@@ -38,16 +93,9 @@ MainWindowCallback(HWND   Window,
             int Y = Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            static DWORD Operation = WHITENESS;
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+            local_persist DWORD Operation = WHITENESS;
             PatBlt(DeviceContext, X, Y, Width, Height, Operation);
-            if(Operation == WHITENESS)
-            {
-                Operation = BLACKNESS;
-            }
-            else
-            {
-                Operation = WHITENESS;
-            }
             EndPaint(Window, &Paint);
         } break;
 
@@ -73,7 +121,7 @@ int WinMain(HINSTANCE Instance,
                                                             // properties that we want our window to have
                                                             // CS_OWNDC Allocates a unique device context for each window in the class.
                                                             // CS_HREDRAW and CS_VREDRAW redraw the window if a movement occured
-        WindowClass.lpfnWndProc = MainWindowCallback; // lpfnWndProc is a pointer to a function that we 
+        WindowClass.lpfnWndProc = Win32MainWindowCallback; // lpfnWndProc is a pointer to a function that we 
                                     // will define that defines how window responds to events
         WindowClass.hInstance = Instance; // hInstance A handle to the instance that contains the window procedure for the class.
     //  WindowClass.hIcon; - we don't need to set the icon now
@@ -97,7 +145,8 @@ int WinMain(HINSTANCE Instance,
                     0);
             if(WindowHandle)
             {
-                for(;;)
+                Running = true;
+                while(Running)
                 {
                     MSG Message;
                     BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
